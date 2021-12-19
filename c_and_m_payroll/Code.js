5cd51559c4977968fb7bd6e078d36e90ee3aa3fe
Manyu Lakhotia <lakemonyew@gmail.com>
2021-12-19 06:51:03 -0600

    Adds cm payroll error handling

--- c_and_m_payroll/Code.js
+++ c_and_m_payroll/Code.js
@@ -2,17 +2,28 @@ function sumEmployeeHoursForRangeOfDays(days) {
   return Array.isArray(days) ?
     days.map(row =>
       row
-        .map(cell => sumEmployeeHoursForDay(cell))
+        .map((cell, i) => sumEmployeeHoursForDay(cell, i))
         .reduce((acc, ele) => acc + ele, 0))
-    : sumEmployeeHoursForDay(days);
+    : sumEmployeeHoursForDay(days, 0);
 }
 
-function sumEmployeeHoursForHourRange(day) {
-  const inputs = day.split('..');
-  if (inputs.length != 2) {
-    return 0;
+function sumEmployeeHoursForHourRange(hourRange, hourRangeIndex, dayIndex) {
+  const errorLabel = `Hour Range Error on Hour Range ${hourRangeIndex + 1} on Day ${dayIndex + 1}: `;
+  const inputs = hourRange.split('..').map(s => s.trim());
+  if (inputs.length !== 2) {
+    if (inputs.length === 1 && inputs[0].toLowerCase() === 'x') {
+      return 0;
+    }
+    throw errorLabel + 'Input Hour Range Value: ' + hourRange + '. Format Notes: hour range in wrong format. Use .. to separate 2 numbers in a range and a , to separate 2 ranges in a single day and an X to mark a day without hours';
   }
-  var begin = Number(inputs[0]);
+
+  const numRe = /^[0-9]*\.?[0-9]*$/;
+  for (n of inputs) {
+    if (n === undefined || !n.match(numRe)) {
+      throw errorLabel + 'number value of ' + n + ' is not a valid number';
+    }
+  }
+  const begin = Number(inputs[0]);
   var end = Number(inputs[1]);
   if (end < begin) {
     end = end + 12;
@@ -20,10 +31,10 @@ function sumEmployeeHoursForHourRange(day) {
   return end - begin;
 }
 
-function sumEmployeeHoursForDay(day) {
+function sumEmployeeHoursForDay(day, dayIndex) {
   const hourRanges = day.split(',');
   return hourRanges
-    .map(hourRange => sumEmployeeHoursForHourRange(hourRange))
+    .map((hourRange, i) => sumEmployeeHoursForHourRange(hourRange, i, dayIndex))
     .reduce((acc, ele) => acc + ele, 0);
 }
 
